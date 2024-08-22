68450e51dbc2febdd08c69cd7a4c1fd842c51a2b
beazinat <beatrizgarodrigues@gmail.com>
2024-08-22 14:54:49 -0300

    :lipstick: Estilização Navbar (transparência e logo)

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,12 @@
 import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
-import logo from '../../assets/images/logo.svg';
+import './Navbar.css'; // Importa o CSS atualizado
+import logo_b from '../../assets/icons/logo_b.png';
+import logo_s from '../../assets/icons/logo_s.png';
+import logo_r from '../../assets/icons/logo_r.png';
 import menuIcon from '../../assets/icons/menu.png';
+import cartIcon from '../../assets/icons/cart.png';
 
 export default function Navbar() {
   const [isVisible, setIsVisible] = useState(true);
@@ -42,7 +46,7 @@ export default function Navbar() {
     navbarCadCat = (
       <Link
         to="/cadastroCategoria"
-        className="block py-2 px-4 text-[#262626] hover:bg-[#82ffba] rounded transition-colors duration-600"
+        className="font-title block py-2 px-4 text-red-600 hover:text-stone-100 hover:bg-zinc-700 rounded transition-colors duration-600"
       >
         Cadastrar Categoria
       </Link>
@@ -50,7 +54,7 @@ export default function Navbar() {
     navbarCadProd = (
       <Link
         to="/cadastroProduto"
-        className="block py-2 px-4 text-[#262626] hover:bg-[#82ffba] rounded transition-colors duration-600"
+        className="font-title block py-2 px-4 text-red-600 hover:text-stone-100 hover:bg-zinc-700 rounded transition-colors duration-600"
       >
         Cadastrar Produtos
       </Link>
@@ -59,7 +63,7 @@ export default function Navbar() {
       <Link
         to="/login"
         onClick={logout}
-        className="block py-2 px-4 text-[#262626] hover:bg-[#82ffba] rounded transition-colors duration-600"
+        className="font-title block py-2 px-4 text-red-600 hover:text-stone-100 hover:bg-zinc-700 rounded transition-colors duration-600"
       >
         Sair
       </Link>
@@ -69,7 +73,7 @@ export default function Navbar() {
       <Link
         to="/login"
         onClick={logout}
-        className="block py-2 px-4 text-[#262626] hover:bg-[#82ffba] rounded transition-colors duration-600"
+        className="font-title block py-2 px-4 text-red-600 hover:text-stone-100 hover:bg-zinc-700 rounded transition-colors duration-600"
       >
         Sair
       </Link>
@@ -79,45 +83,67 @@ export default function Navbar() {
   return (
     <>
       <nav
-        className={`fixed w-full z-30 transition-opacity duration-500 ${
+        className={`fixed w-full transition-opacity duration-500 ${
           isVisible ? 'opacity-100' : 'opacity-0'
-        } bg-zinc-900/80`}
+        } bg-[linear-gradient(to_bottom,_rgba(0,_0,_0,_0.8)_0%,_rgba(0,_0,_0,_0)_100%)]`}
       >
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="/home" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src={logo} className="h-12 mr-3" alt="Joana Logo" />
-          </a>
-          <Link
-            to="/login"
-            className="block py-2 px-4 text-white hover:bg-[#82ffba] rounded transition-colors duration-600"
-          >
-            Login
-          </Link>
-          <Link
-            to="/produtos"
-            className="block py-2 px-4 text-white hover:bg-[#82ffba] rounded transition-colors duration-600"
-          >
-            Produtos
-          </Link>
-          <Link
-            to="/about"
-            className="block py-2 px-4 text-white hover:bg-[#82ffba] rounded transition-colors duration-600"
-          >
-            Sobre nós
-          </Link>
-          <Link
-            to="/contact"
-            className="block py-2 px-4 text-white hover:bg-[#82ffba] rounded transition-colors duration-600"
-          >
-            Contato
-          </Link>
-          <Link
-            to="/carrinho"
-            className="block py-2 px-4 text-white hover:bg-[#82ffba] rounded transition-colors duration-600"
-          >
-            Carrinho [{quantidadeItems}]
-          </Link>
-          <div className="flex md:order-2"></div>
+        <div className="max-w-screen-xl mx-auto flex items-center justify-between px-4 py-2">
+          <div className="flex flex-grow items-center justify-start space-x-4 rtl:space-x-reverse">
+            <Link
+              to="/login"
+              className="font-title py-2 px-4 text-stone-100 hover:text-zinc-900 rounded transition-colors duration-600"
+            >
+              Login
+            </Link>
+            <Link
+              to="/produtos"
+              className="font-title py-2 px-4 text-stone-100 hover:text-zinc-900 rounded transition-colors duration-600"
+            >
+              Produtos
+            </Link>
+          </div>
+          <div className="flex items-center justify-center w-auto">
+            <a href="/home" className="flex items-center">
+              <div className="logoContainer">
+                <img src={logo_b} className="logoImage" alt="Logo Blue" />
+                <img src={logo_r} className="logoImage" alt="Logo Red" />
+                <img src={logo_s} className="logoImage" alt="Logo Silver" />
+              </div>
+            </a>
+          </div>
+          <div className="flex flex-grow items-center justify-end space-x-4 rtl:space-x-reverse">
+            <Link
+              to="/about"
+              className="font-title py-2 px-4 text-stone-100 hover:text-zinc-900 rounded transition-colors duration-600"
+            >
+              Sobre nós
+            </Link>
+            <Link
+              to="/contact"
+              className="font-title py-2 px-4 text-stone-100 hover:text-zinc-900 rounded transition-colors duration-600"
+            >
+              Contato
+            </Link>
+            <Link
+              to="/carrinho"
+              className="relative block text-stone-100 transition-colors duration-600"
+            >
+              <div className="relative inline-block group">
+                <div className="bg-stone-100 rounded-full p-2 transition-opacity duration-600 group-hover:opacity-60">
+                  <img
+                    src={cartIcon}
+                    alt="Carrinho"
+                    className="w-6 h-6 transition-opacity duration-600 group-hover:opacity-60"
+                  />
+                </div>
+                {quantidadeItems > 0 && (
+                  <span className="absolute -top-2 -right-2 bg-red-600 font-body text-stone-100 text-xs rounded-full w-5 h-5 flex items-center justify-center">
+                    {quantidadeItems}
+                  </span>
+                )}
+              </div>
+            </Link>
+          </div>
         </div>
       </nav>
 
@@ -135,42 +161,42 @@ export default function Navbar() {
             <img 
               src={menuIcon} 
               alt="Ícone Menu" 
-              className="w-12 h-12 shadow- rounded-full border-2 border-zinc-900 border-opacity-100 hover:w-14 hover:h-14" // Ajuste o tamanho da imagem aqui
+              className="w-12 h-12 shadow- rounded-full border-2 border-zinc-900 border-opacity-100 hover:w-14 hover:h-14"
             />
           </button>
           <div
             className={`${
               isOpen ? 'block' : 'hidden'
-            } absolute top-full right-0 mt-2 w-64 bg-[#fefcdd] rounded-lg shadow-lg z-20`}
+            } absolute top-full right-0 mt-2 w-64 bg-zinc-900 rounded-lg shadow-lg z-20`}
           >
             <div className="flex flex-col font-medium rounded-lg font-body">
               <Link
                 to="/home"
-                className="block py-2 px-4 text-[#262626] hover:bg-[#82ffba] rounded transition-colors duration-600"
+                className="font-title block py-2 px-4 text-red-600 hover:text-stone-100 hover:bg-zinc-700 rounded transition-colors duration-600"
               >
                 Home
               </Link>
               <Link
                 to="/login"
-                className="block py-2 px-4 text-[#262626] hover:bg-[#82ffba] rounded transition-colors duration-600"
+                className="font-title block py-2 px-4 text-red-600 hover:text-stone-100 hover:bg-zinc-700 rounded transition-colors duration-600"
               >
                 Login
               </Link>
               <Link
                 to="/categorias"
-                className="block py-2 px-4 text-[#262626] hover:bg-[#82ffba] rounded transition-colors duration-600"
+                className="font-title block py-2 px-4 text-red-600 hover:text-stone-100 hover:bg-zinc-700 rounded transition-colors duration-600"
               >
                 Categorias
               </Link>
               <Link
                 to="/produtos"
-                className="block py-2 px-4 text-[#262626] hover:bg-[#82ffba] rounded transition-colors duration-600"
+                className="font-title block py-2 px-4 text-red-600 hover:text-stone-100 hover:bg-zinc-700 rounded transition-colors duration-600"
               >
                 Produtos
               </Link>
               <Link
                 to="/carrinho"
-                className="block py-2 px-4 text-[#262626] hover:bg-[#82ffba] rounded transition-colors duration-600"
+                className="font-title block py-2 px-4 text-red-600 hover:text-stone-100 hover:bg-zinc-700 rounded transition-colors duration-600"
               >
                 Carrinho [{quantidadeItems}]
               </Link>
