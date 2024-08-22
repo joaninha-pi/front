fb4652cbb919c17008826c12fc7c8555ec9287f2
beazinat <beatrizgarodrigues@gmail.com>
2024-08-22 11:06:38 -0300

    :recycle: refactor: Exclusão de Navbar incorreta

---
+++ src/App-DESKTOP-5H2QEB4.tsx
@@ -0,0 +1,47 @@
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Contact from "./pages/Contact/Contact";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/login/Login";
+import { AuthProvider } from './contexts/AuthContext';
+import Cadastro from './pages/Cadastro/Cadastro';
+import "./App.css";
+import ListaCategorias from "./components/categorias/listaCategorias/ListaCategorias";
+import ListaProdutos from "./components/produtos/listaProdutos/ListaProdutos";
+import FormularioCategoria from "./components/categorias/formularioCategoria/FormularioCategoria";
+import FormularioProduto from "./components/produtos/formularioProduto/FormularioProduto";
+import DeletarCategoria from "./components/categorias/deletarCategoria/DeletarCategoria";
+import DeletarProduto from "./components/produtos/deletarProduto/DeletarProduto";
+
+export default function App() {
+  return (
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh] bg-[#FEFCDD] text-gray-800">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/produtos" element={<ListaProdutos />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastroProduto" element={<FormularioProduto />} />
+              <Route path="/deletarProduto/:id" element={<DeletarProduto />} />
+              <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
+              <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
+              <Route path="/editarProduto/:id" element={<FormularioProduto />} />
+              <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
+    </>
+  );
+}
---
+++ src/pages/Home/Home-DESKTOP-5H2QEB4.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                <h1 style={{ paddingTop: '250px', paddingBottom: '50px', fontSize: '30px', fontWeight: 'bolder'}}>Home</h1>
+            </div>
+        </>
+    )
+}
