db569780f22f3cb5b1f295d732059d9eff720062
Ney Robson <nrobson018@gmail.com>
2024-08-23 12:32:30 -0300

    :lipstick: subindo alteraçoes do navbar e footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -26,13 +26,13 @@ export default function Footer() {
         <>
             <div className={`fixed justify-center bottom-0 w-full transition-opacity duration-500 ${isVisible ? 'opacity-100' : 'opacity-0'} bg-gradient-to-t from-black/80 to-transparent text-neutral-100`}>
                 <div className="container flex flex-col items-center py-4 pl-44">
-                    <p className="text-xl font-bold text-red-300">Joana | Copyright: Generation Brasil</p>
-                    <p className="text-lg text-red-300">Nos acompanhe nas redes!</p>
+                    <p className="text-xl font-bold text-red-400">Joana | Copyright: Generation Brasil</p>
+                    <p className="text-lg text-red-400">Nos acompanhe nas redes!</p>
                     <div className="flex gap-2 mt-2">
-                        <a className="text-red-300 hover:text-red-700 transition-colors duration-300" href="https://github.com/joaninha-pi">
+                        <a className="text-red-400 hover:text-red-700 transition-colors duration-300" href="https://github.com/joaninha-pi">
                             <GithubLogo size={32} />
                         </a>
-                        <a className="text-red-300 hover:text-red-700 transition-colors duration-300" href="https://linktr.ee/joana_pi">
+                        <a className="text-red-400 hover:text-red-700 transition-colors duration-300" href="https://linktr.ee/joana_pi">
                             <At size={32} />
                         </a>
                     </div>
