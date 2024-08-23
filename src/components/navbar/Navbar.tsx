ba399c69790ba33ba9034522c79f2bbf089483b3
lucas silva <lucas.vasconcelosop@hotmail.com>
2024-08-23 06:59:49 -0300

    :lipstick: update na centralização do navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -152,7 +152,7 @@ export default function Navbar() {
                 Produtos
               </Link>
             </div>
-            <div className="flex items-center justify-center w-auto pl-14">
+            <div className="flex items-center justify-center w-auto" style={{ paddingLeft: '78px' }}>
               <a href="/home" className="flex items-center">
                 <div className="logoContainer">
                   <img src={logo_b} className="logoImage" alt="Logo Blue" />
