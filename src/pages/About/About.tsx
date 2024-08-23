5e62f671abbf4479059ea0c3fb116712af762a1f
Renata Rainho <renata.rainho95@gmail.com>
2024-08-22 22:59:35 -0300

    :lipstick: Ajustes de backgorund

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,24 +1,14 @@
-export default function About() {
+
+import React from 'react';
+import TeamSection from './TeamSection';
+
+const AboutUs = () => {
     return (
-        <>
-            <div className="fundoLogao">
-                <div className='pt-24'></div>
-                <h1 style={{ textAlign: 'center', paddingTop: '30px', paddingBottom: '5px', fontSize: '30px', fontWeight: 'bold' }}>Sobre nós</h1>
-                <div style={{ paddingTop: '5px', paddingBottom: '50px', fontSize: '30px', fontWeight: "unset" }}>
-                    <p style={{ textAlign: 'center' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore veritatis sit iure libero,
-                        voluptate recusandae ipsa nisi est consequatur. Necessitatibus corrupti numquam aut omnis, dolorum
-                        at saepe quia dicta aliquid!</p>
-                    <p style={{ textAlign: 'center' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore veritatis sit iure libero,
-                        voluptate recusandae ipsa nisi est consequatur. Necessitatibus corrupti numquam aut omnis, dolorum
-                        at saepe quia dicta aliquid!</p>
-                    <p style={{ textAlign: 'center' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore veritatis sit iure libero,
-                        voluptate recusandae ipsa nisi est consequatur. Necessitatibus corrupti numquam aut omnis, dolorum
-                        at saepe quia dicta aliquid!</p>
-                    <p style={{ textAlign: 'center' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore veritatis sit iure libero,
-                        voluptate recusandae ipsa nisi est consequatur. Necessitatibus corrupti numquam aut omnis, dolorum
-                        at saepe quia dicta aliquid!</p>
+    <div>
+      {/* Outras seções da página "Sobre Nós" */}
+      <TeamSection />
                 </div>
-            </div>
-        </>
-    )
-}
+            );
+};
+
+export default AboutUs;
