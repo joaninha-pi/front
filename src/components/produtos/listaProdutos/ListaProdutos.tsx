e3355fe5266b89cd12b1dd2856ca100b80bda7c9
beazinat <beatrizgarodrigues@gmail.com>
2024-08-23 08:51:16 -0300

    :lipstick: Estilização Modal Produtos

--- src/components/produtos/listaProdutos/ListaProdutos.tsx
+++ src/components/produtos/listaProdutos/ListaProdutos.tsx
@@ -43,7 +43,7 @@ function ListaProdutos() {
             />
           </div>
         )}
-        <div className='container mx-auto py-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4'>
           {produtos.map((produto) => (
             <CardProduto key={produto.id} produto={produto} />
           ))}
