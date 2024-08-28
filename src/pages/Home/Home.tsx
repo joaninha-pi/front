0ea339fb32d53c20ae5dd94bc0f7ccbffaa27d1f
Helt-0 <lucas.vasconcelosop@hotmail.com>
2024-08-28 17:25:40 -0300

    Correção na home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -14,17 +14,17 @@ export default function Home() {
             {/* Seção de Destaques */}
             <section className="highlights-section">
                 <div className="highlight">
-                    <h2>Novidades</h2>
+                    <h2 className='font-title'>Novidades</h2>
                     <br></br>
                     <p>Descubra os últimos lançamentos na Joana. Produtos exclusivos e estilos únicos para você.</p>
                 </div>
                 <div className="highlight">
-                    <h2>Promoções Imperdíveis</h2>
+                    <h2 className='font-title'>Promoções Imperdíveis</h2>
                     <br></br>
                     <p>Descontos de até 50% em itens selecionados. Não perca essa oportunidade!</p>
                 </div>
                 <div className="highlight">
-                    <h2>Entrega Rápida na região da Capital</h2>
+                    <h2 className='font-title'>Entrega Rápida na região da Capital</h2>
                     <br></br>
                     <p>Receba seu pedido em casa com toda a comodidade. </p>
                     <p>Frete grátis nas compras acima de R$150.</p>
@@ -32,22 +32,20 @@ export default function Home() {
             </section>
 
             {/* Imagem e texto central */}
-            <section className="imagem-texto">
-                <img className="hover:scale-110" src={"https://media.discordapp.net/attachments/1262942566370775061/1275829119220711547/UniversalUpscaler_fc4f7347-069b-452e-9ed7-9388e5607214-removebg.png?ex=66cfe1b5&is=66ce9035&hm=fc3413cd8f59e2dd149535e3b532a3b3c7fec769332831a84e19cabacd22312d&=&format=webp&quality=lossless&width=416&height=416"} />
+            <section className="pt-20 imagem-texto">
+                <img className="size-5/6 hover:scale-110" src={"https://media.discordapp.net/attachments/1262942566370775061/1275829119220711547/UniversalUpscaler_fc4f7347-069b-452e-9ed7-9388e5607214-removebg.png?ex=66cfe1b5&is=66ce9035&hm=fc3413cd8f59e2dd149535e3b532a3b3c7fec769332831a84e19cabacd22312d&=&format=webp&quality=lossless&width=416&height=416"} />
                 <div className="texto">
-                    <h1>Mas por quê Joana?</h1>
-                    <p>Nossa relação com as joaninhas vão para além dos nossos produtos e elas tem muitas histórias para nos contar. Em muitas culturas elas carregam consigo a representação de felicidade, amor e harmonia.</p>
-                    <p>Joaninhas são consideradas como símbolo de boa sorte e aqui na Joana sabemos o quanto ela é valiosa para o ecossistema. Com o seu apetite voraz ela faz o controle de pragas e pulgões e também se alimentam de néctar.
-                        Suas plantas favoritas são as que retêm água como alfaces, couve e flores como tulipas e lírios.</p>
-                    <p>Para nós o formato das joaninhas são fofos e encantadores mas seu formato e cores têm a função de intimidar os predadores passando uma mensagem que seu gosto é ruim e pode ser tóxica.
-                        Contudo o formato como a conhecemos não é o primeiro, ela é uma larva que passa por um estágio de metamorfose como as borboletas para assim ter esse formato de besouro.</p>
-                    <p>Estando alinhado as nossas diretrizes de produção consciente e sustentável.</p>
+                    <h1 className='text-2xl font-title'>Mas por quê Joana?</h1>
+                    <p className='text-justify font-body'>Nossa relação com as joaninhas vai além dos nossos produtos, e elas têm muitas histórias para nos contar. Em muitas culturas, elas carregam consigo a representação de felicidade, amor e harmonia.</p>
+                    <p className='text-justify font-body'>As joaninhas são consideradas símbolos de boa sorte, e aqui na Joana sabemos o quanto elas são valiosas para o ecossistema. Com seu apetite voraz, elas controlam pragas e pulgões e também se alimentam de néctar. Suas plantas favoritas são aquelas que retêm água, como alface e couve, além de flores como tulipas e lírios.</p>
+                    <p className='text-justify font-body'>Para nós, o formato das joaninhas é fofo e encantador, mas suas cores e formato têm a função de intimidar os predadores, passando a mensagem de que seu gosto é ruim e pode ser tóxico. No entanto, o formato que conhecemos não é o primeiro. A joaninha começa como uma larva e passa por um estágio de metamorfose, semelhante ao das borboletas, para adquirir o formato de besouro. Estando alinhado as nossas diretrizes de produção consciente e sustentável.</p>
+                    
                 </div>
             </section>
 
             
-            <section className="testimonials-section">
-                <h1>Depoimento dos nossos clientes:</h1>
+            <section className="pt-40 testimonials-section">
+                <h1 className='text-2xl font-title'>Depoimento dos nossos clientes:</h1>
                 <div className="testimonial">
                     <p>"Amei a qualidade dos produtos da Joana! Com certeza vou comprar mais vezes."</p>
                     <span>- Maria S.</span>
