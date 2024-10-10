8062ec4293678effa394a5915f438b4dd9c792e9
beazinat <beatrizgarodrigues@gmail.com>
2024-10-10 03:36:28 -0300

    :lipstick: feat: Estilização cadastro

--- src/pages/Cadastro/Cadastro.tsx
+++ src/pages/Cadastro/Cadastro.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Usuario from '../../models/Usuario';
 import { cadastrarUsuario } from '../../services/Service';
 import { toastAlerta } from '../../utils/toastAlerta';
-import { RotatingLines, RevolvingDot } from 'react-loader-spinner';
+import { RotatingLines } from 'react-loader-spinner';
 
 function Cadastro() {
   let navigate = useNavigate();
@@ -16,7 +16,7 @@ function Cadastro() {
     senha: '',
     foto: ''
   });
-  
+
   const [usuarioResposta, setUsuarioResposta] = useState<Usuario>({
     id: 0,
     nome: '',
@@ -25,7 +25,7 @@ function Cadastro() {
     foto: ''
   });
 
-  const [isLoading, setIsLoading] = useState<boolean>(false); // Novo estado para controle do loading
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     if (usuarioResposta.id !== 0) {
@@ -50,16 +50,16 @@ function Cadastro() {
 
   async function cadastrarNovoUsuario(e: ChangeEvent<HTMLFormElement>) {
     e.preventDefault();
-    
+
     if (confirmaSenha === usuario.senha && usuario.senha.length >= 8) {
-      setIsLoading(true); // Inicia o loading
+      setIsLoading(true);
       try {
         await cadastrarUsuario(`/usuarios/cadastrar`, usuario, setUsuarioResposta);
         toastAlerta('Usuário cadastrado com sucesso', 'sucesso');
       } catch (error) {
         toastAlerta('Erro ao cadastrar o usuário', 'erro');
       } finally {
-        setIsLoading(false); // Finaliza o loading
+        setIsLoading(false);
       }
     } else {
       toastAlerta('Dados inconsistentes. Verifique as informações de cadastro.', 'erro');
@@ -77,7 +77,58 @@ function Cadastro() {
           <h2 className="text-2xl font-title font-extrabold text-red-700">Crie sua conta</h2>
         </div>
         <form className="flex font-content font-semibold flex-col gap-4" onSubmit={cadastrarNovoUsuario}>
-          {/* Outros campos de entrada */}
+          {/* Campo de entrada para nome */}
+          <div className="flex flex-col">
+            <label htmlFor="nome" className="text-[#25433C]">Nome</label>
+            <input
+              type="text"
+              id="nome"
+              name="nome"
+              placeholder="Nome"
+              className="p-2 border-b-2 bg-[#DEE6BE] border-red-700 focus:outline-none focus:border-[#9ed582] transition duration-300"
+              value={usuario.nome}
+              onChange={atualizarEstado}
+            />
+          </div>
+          {/* Campo de entrada para usuário */}
+          <div className="flex flex-col">
+            <label htmlFor="usuario" className="text-[#25433C]">Usuário</label>
+            <input
+              type="text"
+              id="usuario"
+              name="usuario"
+              placeholder="Usuário"
+              className="p-2 border-b-2 bg-[#DEE6BE] border-red-700 focus:outline-none focus:border-[#9ed582] transition duration-300"
+              value={usuario.usuario}
+              onChange={atualizarEstado}
+            />
+          </div>
+          {/* Campo de entrada para senha */}
+          <div className="flex flex-col">
+            <label htmlFor="senha" className="text-[#25433C]">Senha</label>
+            <input
+              type="password"
+              id="senha"
+              name="senha"
+              placeholder="Senha"
+              className="p-2 border-b-2 bg-[#DEE6BE] border-red-700 focus:outline-none focus:border-[#9ed582] transition duration-300"
+              value={usuario.senha}
+              onChange={atualizarEstado}
+            />
+          </div>
+          {/* Campo de entrada para URL da foto */}
+          <div className="flex flex-col">
+            <label htmlFor="foto" className="text-[#25433C]">URL da Foto</label>
+            <input
+              type="text"
+              id="foto"
+              name="foto"
+              placeholder="Insira a URL da sua foto"
+              className="p-2 border-b-2 bg-[#DEE6BE] border-red-700 focus:outline-none focus:border-[#9ed582] transition duration-300"
+              value={usuario.foto}
+              onChange={atualizarEstado}
+            />
+          </div>
           <button type='submit' className="mt-4 bg-[#25433C] text-[#DEE6BE] py-2 rounded hover:bg-[#9ed582] hover:text-[#25433C] transition duration-300 flex justify-center items-center">
             {isLoading ? (
               <RotatingLines strokeColor="white" strokeWidth="5" animationDuration="0.75" width="24" visible={true} />
