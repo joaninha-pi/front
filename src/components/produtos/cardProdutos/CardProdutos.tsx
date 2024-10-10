e55fc45a5299cc21f327ec6b838badf3a437a957
beazinat <beatrizgarodrigues@gmail.com>
2024-10-10 05:26:48 -0300

    :bug: fix: Melhoria nos cards de produtos

--- src/components/produtos/cardProdutos/CardProdutos.tsx
+++ src/components/produtos/cardProdutos/CardProdutos.tsx
@@ -13,13 +13,6 @@ function CardProduto({ produto }: CardProdutoProps) {
   const { usuario } = useContext(AuthContext);
   const [quantidade, setQuantidade] = useState(0);
 
-  const exibirPeso = (peso: number) => {
-    if (peso >= 1000) {
-      return `${(peso / 1000).toFixed(2)} kg`;
-    }
-    return `${peso} g`;
-  };
-
   const handleAdicionarAoCarrinho = () => {
     if (quantidade > 0) {
       adicionarProduto({ ...produto, quantidade });
@@ -33,7 +26,7 @@ function CardProduto({ produto }: CardProdutoProps) {
   const isAdmin = usuario && usuario.usuario.endsWith('@root.com');
 
   return (
-    <div className="bg-[#9ED582] rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105">
+    <div className="bg-[#9ED582] rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105 flex flex-col h-full">
       <Link to={`/produtos`}>
         <img
           src={produto.image}
@@ -41,7 +34,7 @@ function CardProduto({ produto }: CardProdutoProps) {
           className="w-full h-40 object-cover transition duration-300 ease-in-out transform hover:scale-110"
         />
       </Link>
-      <div className="p-4">
+      <div className="p-4 flex flex-col flex-grow">
         <h3 className="text-xl font-semibold">{produto.nome}</h3>
         <p className="text-lg font-bold mt-2 text-[#AB100B]">R$ {produto.preco.toFixed(2)}</p>
 
@@ -65,23 +58,26 @@ function CardProduto({ produto }: CardProdutoProps) {
           </button>
         </div>
 
-        <button 
-          onClick={handleAdicionarAoCarrinho} 
-          className="w-full mt-4 bg-[#25433C] text-[#DEE6BE] font-bold py-2 rounded-lg hover:bg-[#9ed582] transition transform hover:scale-105 hover:text-[#25433C]">
-          Adicionar ao Carrinho
-        </button>
+        {/* Botão de adicionar ao carrinho */}
+        <div className="flex-grow flex items-end mt-auto">
+          <button 
+            onClick={handleAdicionarAoCarrinho} 
+            className="mt-4 mb-2 w-full bg-[#25433C] text-[#DEE6BE] font-bold py-2 rounded-lg hover:bg-[#9ed582] transition transform hover:scale-105 hover:text-[#25433C]">
+            Adicionar ao Carrinho
+          </button>
+        </div>
 
         {/* Ações administrativas */}
         {isAdmin && (
           <div className="flex justify-between mt-2">
             <Link 
               to={`/editarProduto/${produto.id}`} 
-              className="text-blue-500 hover:underline flex items-center">
+              className="text-blue-500 hover:underline flex items-center w-1/2 justify-center">
               <FaEdit className="mr-1" /> Editar
             </Link>
             <Link 
               to={`/deletarProduto/${produto.id}`} 
-              className="text-red-600 hover:underline flex items-center">
+              className="text-red-600 hover:underline flex items-center w-1/2 justify-center">
               <FaTrashAlt className="mr-1" /> Deletar
             </Link>
           </div>
